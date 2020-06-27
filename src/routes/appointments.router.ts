204b4fdf35ce0a7a47b6c4bc7818aadd6e9121e9
joao vitor <joao3klb@hotmail.com>
2020-06-27 20:18:43 -0300

    models e repository

--- src/routes/appointments.router.ts
+++ src/routes/appointments.router.ts
@@ -1,19 +1,35 @@
-import { Router} from 'express';
-import { uuid } from 'uuidv4';
+import { Router, request} from 'express';
+import { startOfHour, parseISO } from 'date-fns'
+import AppointmentRepository from '../repositories/AppointmentsRepository';
+
 const appointmentRouter = Router();
 
-const appointments = [];
+const appointmentRepository = new AppointmentRepository();
+
+appointmentRouter.get('/', (request,response) => {
+  const appointments = appointmentRepository.all();
+
+  return response.json(appointments);
+})
+
 
 appointmentRouter.post('/', (request,response) => {
 
   const { provider , date } = request.body;
 
-  const appointment = {
-    id: uuid(),
+  const parseDate = startOfHour(parseISO(date));
+
+  const findAppointmentInSameDate = appointmentRepository.findByDate(parseDate);
+
+  if(findAppointmentInSameDate){
+    return response.status(400).json({error: 'This appointment is already booked'})
+  }
+
+
+  const appointment = appointmentRepository.create({
     provider,
-    date
-  };
-  appointments.push(appointment)
+    date:parseDate,
+  });
   return response.json(appointment);
 });
 export default appointmentRouter;
