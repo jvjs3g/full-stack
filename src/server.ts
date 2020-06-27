a0cfdd0bc70cb39f557c69bae228973ff6402a7a
joao vitor <joao3klb@hotmail.com>
2020-06-27 14:01:30 -0300

    agendamento

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,9 @@
 import express from 'express';
-
+import routes from './routes/index';
 const app = express();
 
 app.use(express.json());
-app.get('/', (request, response) => {
-  return response.json({ message: 'Hello GoStack' });
-});
+app.use(routes)
 
 app.listen(3333, () =>{
   console.log('Server subiu !!!');
