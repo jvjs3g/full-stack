503d40b6f6234a20f6dabe10a1ede4b663ae527a
joao vitor <joao3klb@hotmail.com>
2020-09-13 20:52:48 -0300

    Rotas e controllers

--- src/modules/appointments/infra/http/routes/providers.routes.ts
+++ src/modules/appointments/infra/http/routes/providers.routes.ts
@@ -4,14 +4,19 @@ import { container } from 'tsyringe';
 
 import ProviderController from '../controllers/ProvidersController';
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
-
+import ProviderMonthAvailabilityController from '../controllers/PorviderMonthAvailabilityController';
+import ProviderDayAvailabilityController from '../controllers/PorviderDayAvailabilityController';
 
 const ProvidersRouter = Router();
 
 const providerController = new ProviderController();
+const providerManthAvailabilityController = new ProviderMonthAvailabilityController();
+const providerDayAvailabilityController = new ProviderDayAvailabilityController();
 
 ProvidersRouter.use(ensureAuthenticated);
 
 ProvidersRouter.get('/', providerController.index);
+ProvidersRouter.get('/:provider_id/month-availability', providerManthAvailabilityController.index);
+ProvidersRouter.get('/:provider_id/day-availability', providerDayAvailabilityController.index);
 
 export default ProvidersRouter;
